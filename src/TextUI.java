ecee6a96ffbf538ffc8c5e88330d288bf1f84aa3
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-06-09 21:10:36 -0400

    Create basic request and response

---
+++ src/TextUI.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class TextUI {
+    private final Scanner scanner;
+    private final AppService service;
+
+    public TextUI(AppService service) {
+        this.scanner = new Scanner(System.in);
+        this.service = service;
+    }
+
+    public void start() {
+
+        // loop until program ends
+        while (true) {
+            // receive user input
+            // process user input
+            // output result
+            String request = this.inputRequest();
+            this.processRequest(request);
+            if (request.equals("exit")) {
+                break;
+            }
+        }
+
+    }
+
+    public String inputRequest() {
+        return this.scanner.nextLine();
+    }
+
+    public void processRequest(String request) {
+        // future - these cases should call a service method to process it
+        switch (request) {
+            case "new":
+                System.out.println("---NEW RELEASES---");
+                System.out.println("Mountains [Sia, Diplo, Labrinth]");
+                System.out.println("Runaway [Lil Peep]");
+                System.out.println("The Greatest Show [Panic! At The Disco]");
+                System.out.println("All Out Life [Slipknot]");
+                break;
+            case "featured":
+                System.out.println("---FEATURED---");
+                System.out.println("Mellow Morning");
+                System.out.println("Wake Up and Smell the Coffee");
+                System.out.println("Monday Motivation");
+                System.out.println("Songs to Sing in the Shower");
+                break;
+            case "categories":
+                System.out.println("---CATEGORIES---");
+                System.out.println("Top Lists");
+                System.out.println("Pop");
+                System.out.println("Mood");
+                System.out.println("Latin");
+                break;
+            case "playlists Mood":
+                System.out.println("---MOOD PLAYLISTS---");
+                System.out.println("Walk Like A Badass ");
+                System.out.println("Rage Beats");
+                System.out.println("Arab Mood Booster");
+                System.out.println("Sunday Stroll");
+                break;
+            case "exit":
+                System.out.println("---GOODBYE!---");
+                break;
+            default:
+                System.out.println("No cases found");
+        }
+    }
+
+}

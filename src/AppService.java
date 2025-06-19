18f8791de0f1b2a8aa37021941545f46687dcb3a
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-06-18 20:48:15 -0400

    Create POST request for access token

--- src/AppService.java
+++ src/AppService.java
@@ -2,11 +2,13 @@ public class AppService {
     private boolean isAuthorized;
     private final String clientID;
     private final String redirectURI;
+    private final String serverPath;
 
     public AppService() {
         this.isAuthorized = false;
         this.clientID = "0e7c212c5dce45fb91dd4dc33be57b30";
         this.redirectURI = "http://127.0.0.1:8080/callback";
+        this.serverPath = "https://accounts.spotify.com";
     }
 
     public void setIsAuthorized(boolean value) {
@@ -18,8 +20,12 @@ public class AppService {
     }
 
     public String getAuthLink() {
-        return "https://accounts.spotify.com/authorize?client_id="
-                + clientID + "&redirect_uri=" + redirectURI
+        return this.serverPath + "/authorize?client_id="
+                + this.clientID + "&redirect_uri=" + this.redirectURI
                 + "&response_type=code";
+    }
+
+    public String getRedirectURI() {
+        return this.redirectURI;
     }
 }
--- src/AuthClient.java
+++ src/AuthClient.java
@@ -1,15 +1,38 @@
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
 
 public class AuthClient {
     private final HttpClient client;
+    private final String clientID;
+    private final String clientSecret;
 
     public AuthClient() {
         this.client = HttpClient.newBuilder().build();
+        this.clientID = "0e7c212c5dce45fb91dd4dc33be57b30";
+        this.clientSecret = "9e61b7d69d3f47caa437c4cadf816208";
     }
 
-    public void getAccessToken() {
+    public void getAccessToken(String redirectURI) {
+        String auth = this.clientID + ":" + clientSecret;
+        String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes());
 
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://accounts.spotify.com/api/token"))
+                .header("Authorization", "Basic " + encodedAuth)
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString("grant_type=authorization_code"))
+                .build();
 
+        try {
+            HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response: " + response.body());
+        } catch (Exception e) {
+            System.err.println("Exception occurred: " + e.getMessage());
+            e.printStackTrace(); // Optional: helpful for debugging
+        }
     }
 
 }
